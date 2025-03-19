dab549c91e2cff35b82058f6d6b26effc6eb03bf
ilsimi1066 <diegosimi2k5@gmail.com>
2025-03-19 20:45:25 +0100

    added sprites + animations

---
+++ ClayVenture/Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float horizontalInput;
+    float moveSpeed = 5f;
+    bool isFacingLeft = false;
+    float jumpPower = 4f;
+    bool isGrounded = false;
+
+    Rigidbody2D rb;
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        FlipSprite();
+
+        if (Input.GetButtonDown("Jump") && !isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+            isGrounded = false;
+            animator.SetBool("isJumping", !isGrounded);
+        }
+    }
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
+        animator.SetFloat("xVelocity", Math.Abs(rb.velocity.x));
+        animator.SetFloat("yVelocity", rb.velocity.y);
+    }
+
+    void FlipSprite()
+    {
+        if (isFacingLeft && horizontalInput < 0f || !isFacingLeft && horizontalInput > 0f)
+        {
+            isFacingLeft = !isFacingLeft;
+            Vector3 ls = transform.localScale;
+            ls.x *= -1f;
+            transform.localScale = ls;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        isGrounded = true;
+        animator.SetBool("isJumping", !isGrounded);
+    }
+}
